6fdb93279baf2cc47d4295f249d605c6577b1e05
Amrit Sinha <purpleplots@gmail.com>
2023-05-19 13:23:27 +0530

    init - initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="App">Budget Buddy</div>
+    </>
+  );
+}
+
+export default App;
